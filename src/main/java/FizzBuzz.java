84a9701470c8d12985db0b56dffe15fa929b1d8a
Sachin Tripathi <sachin@incubyte.co>
2022-02-08 11:50:06 +0530

    Mock PrintStream

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,19 +1,29 @@
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class FizzBuzz {
     private final int x;
-
-    public FizzBuzz(int x) {
+    PrintStream out;
+    public FizzBuzz(int x, PrintStream printStream) {
         this.x = x;
+        this.out = printStream;
     }
 
-    public String give() {
-        if (x % 3 == 0 && x % 5 == 0) {
+    public String give(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
-        } else if (x % 5 == 0) {
+        } else if (number % 5 == 0) {
             return "Buzz";
-        } else if (x % 3 == 0) {
+        } else if (number % 3 == 0) {
             return "Fizz";
         } else {
-            return String.valueOf(x);
+            return String.valueOf(number);
+        }
+    }
+
+    public void printall() {
+        for (int i = 1; i <= x; i++) {
+            out.println(give(i));
         }
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,34 +1,51 @@
-import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class FizzBuzzTest {
+import java.io.PrintStream;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+class FizzBuzzTest {
 
     @Test
-    public void test_for_multiple_of_three() {
-        FizzBuzz fizzbuzz = new FizzBuzz(3);
-        Assertions.assertThat(fizzbuzz.give()).isEqualTo("Fizz");
+    void test_for_multiple_of_three() {
+        FizzBuzz fizzbuzz = new FizzBuzz(3, printStream);
+        Assertions.assertThat(fizzbuzz.give(3)).isEqualTo("Fizz");
     }
 
     @Test
-    public void test_for_multiple_of_five() {
-        FizzBuzz fizzbuzz = new FizzBuzz(5);
-        Assertions.assertThat(fizzbuzz.give()).isEqualTo("Buzz");
+    void test_for_multiple_of_five() {
+        FizzBuzz fizzbuzz = new FizzBuzz(5, printStream);
+        Assertions.assertThat(fizzbuzz.give(5)).isEqualTo("Buzz");
     }
 
     @Test
-    public void test_for_multiple_of_combine() {
-        FizzBuzz fizzbuzz = new FizzBuzz(15);
-        Assertions.assertThat(fizzbuzz.give()).isEqualTo("FizzBuzz");
+    void test_for_multiple_of_combine() {
+        FizzBuzz fizzbuzz = new FizzBuzz(15, printStream);
+        Assertions.assertThat(fizzbuzz.give(15)).isEqualTo("FizzBuzz");
     }
 
     @Test
-    public void test_for_no_multiple() {
-        FizzBuzz fizzbuzz = new FizzBuzz(7);
-        Assertions.assertThat(fizzbuzz.give()).isEqualTo("7");
+    void test_for_no_multiple() {
+        FizzBuzz fizzbuzz = new FizzBuzz(7, printStream);
+        Assertions.assertThat(fizzbuzz.give(7)).isEqualTo("7");
     }
 
+    @Mock
+    PrintStream printStream;
+    @Test
+    void test_print_all_fizz_budd_till_n() {
+        FizzBuzz fizzBuzz = new FizzBuzz(15, printStream);
+        fizzBuzz.printall();
+        verify(printStream, times(1)).println("FizzBuzz");
+        verify(printStream, times(4)).println("Fizz");
+        verify(printStream, times(2)).println("Buzz");
+    }
 
 }
